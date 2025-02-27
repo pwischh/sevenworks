6d255ce522e1de3065244c4d6c330d03e91ce177
alexlautin <alexalautin@gmail.com>
2025-02-27 16:39:10 -0500

    change website name to "Sevenworks"

--- sevenworks/src/app/page.tsx
+++ sevenworks/src/app/page.tsx
@@ -1,11 +1,15 @@
 import Navbar from "./landing/navbar";
 import Landing from "./landing/landing";
 import TemplatePreview from "./landing/templatePrev";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div className = "flex flex-col w-full h-fit justify-start">
       <Navbar />
+      <Head>
+        <title>Sevenworks</title>
+      </Head>
       <div className = "flex flex-col w-full h-fit justify-start pt-[75px]">
         <Landing />
         <TemplatePreview />
