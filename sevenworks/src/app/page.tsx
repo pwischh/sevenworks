efec679077586b820e3c026f4d7d4df6ca2438cf
alexlautin <alexalautin@gmail.com>
2025-02-27 18:18:39 -0500

    update favicon to book icon

--- sevenworks/src/app/page.tsx
+++ sevenworks/src/app/page.tsx
@@ -5,12 +5,13 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className = "flex flex-col w-full h-fit justify-start">
+    <div className="flex flex-col w-full h-fit justify-start">
       <Navbar />
       <Head>
         <title>Sevenworks</title>
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className = "flex flex-col w-full h-fit justify-start pt-[75px]">
+      <div className="flex flex-col w-full h-fit justify-start pt-[75px]">
         <Landing />
         <TemplatePreview />
       </div>
