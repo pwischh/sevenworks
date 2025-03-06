f7babccea275b91ae23285bcf91487dc79c24e08
alexlautin <alexalautin@gmail.com>
2025-03-06 10:17:52 -0500

    redo changes that were deleted in lost commits

---
+++ sevenworks/src/app/landing/landing.tsx
@@ -0,0 +1,46 @@
+import { FaRegCircleCheck } from "react-icons/fa6";
+import "./landing.css";
+import ImageGrid from "./imagegrid.js";
+
+export default function Landing() {
+    return (
+        <div className="flex flex-row w-full min-h-screen justify-center items-center bg-gradient-to-b from-navy to-darkRed overflow-y-auto">
+            <div className="flex flex-col w-full h-full justify-center items-start gap-[32px] px-[100px] pb-[75px] text-offWhite">
+                <h1 className="max-w-[700px] text-[60px] font-extrabold leading-tight tracking-wide">
+                    Focus on your future, not formatting.
+                </h1>
+                <div className="w-[70%] h-[2px] bg-offWhite"></div>
+                <div className="flex flex-col w-full h-fit place-content-center gap-[24px] py-[24px]">
+                    <div className="flex flex-row justify-start items-center gap-[24px]">
+                        <FaRegCircleCheck className="text-lightRed w-[30px] h-[30px]"/>
+                        <p className="italic text-[22px] font-semibold text-offWhite">
+                            Choose a fully customizable template
+                        </p>
+                    </div>
+                    <div className="flex flex-row justify-start items-center gap-[24px]">
+                        <FaRegCircleCheck className="text-lightRed w-[30px] h-[30px]"/>
+                        <p className="italic text-[22px] font-semibold text-offWhite">
+                            Pinpoint your weaknesses with AI
+                        </p>
+                    </div>
+                    <div className="flex flex-row justify-start items-center gap-[24px]">
+                        <FaRegCircleCheck className="text-lightRed w-[30px] h-[30px]"/>
+                        <p className="italic text-[22px] font-semibold text-offWhite">
+                            Save files to your account to edit later
+                        </p>
+                    </div>
+                </div>
+                <div className="flex flex-row w-fit h-fit gap-[12px] pl-[2px]">
+                    <a href="/register/signup" className="font-medium text-[20px] text-offWhite hover:underline">
+                        Get Started
+                    </a>
+                </div>
+            </div>
+            <div className="flex flex-col w-[1000px] h-full justify-center items-center p-[50px] pr-[100px] mt-[-80px]"> {/* Adjusted margin-top to raise the images */}
+                <div className="min-h-screen flex items-center justify-center">
+                    <ImageGrid />
+                </div>
+            </div>
+        </div>
+    );
+}
--- sevenworks/src/app/page.tsx
+++ sevenworks/src/app/page.tsx
@@ -1,12 +1,19 @@
-import Navbar from "./home/navbar";
-import Landing from "./home/landing";
+import Navbar from "./landing/navbar";
+import Landing from "./landing/landing";
+import TemplatePreview from "./landing/templatePrev";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className = "flex flex-col w-full h-fit justify-start bg-navy">
+    <div className="flex flex-col w-full h-fit justify-start">
       <Navbar />
-      <div className = "flex flex-col w-full h-fit justify-start pt-[75px]">
+      <Head>
+        <title>Sevenworks</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="flex flex-col w-full h-fit justify-start pt-[75px]">
         <Landing />
+        <TemplatePreview />
       </div>
     </div>
   );
