da4391598ca83caa33f58c8e930def58ea89e498
alexlautin <alexalautin@gmail.com>
2025-03-07 12:38:59 -0500

    edits to editor navbar and page, add icons

--- sevenworks/src/app/editor/navbar.tsx
+++ sevenworks/src/app/editor/navbar.tsx
@@ -5,17 +5,45 @@ export default function Navbar() {
         <div className="bg-white">
             <div className="pt-18 pb-18 pl-90 pr-90">
                 <nav className="text-black flex flex-row gap-10 p-3 w-fit text-nowrap font-semibold mx-auto bg-[#E6E6E6] rounded-full p-4">
-                    <a href="#">Zoom</a>
-                    <a href="#">Font</a>
-                    <a href="#">Font Size</a>
-                    <a href="#">Bullet</a>
-                    <a href="#">Link</a>
-                    <a href="#">Save</a>
-                    <a href="#">Bullet</a>
-                    <a href="#">Reload</a>
-                    <a href="#">Download</a>
-                    <a href="#">Settings</a>
-                    <a href="#">Logout</a>
+                    <span className="flex items-center gap-2">
+                        <span>SevenWorks</span>
+                    </span>
+                    <span className="flex items-center gap-2">
+                        <img src="/zoom-out.svg" alt="zoom out" className="w-6 h-6" />
+                        <span>Zoom</span>
+                        <img src="/zoom-in.svg" alt="zoom in" className="w-6 h-6" />
+                    </span>
+                    <span className="self-center h-6 border-r border-black"></span>
+                    <span className="flex items-center gap-2">
+                        <span>Font</span>
+                        <img src="/chevron-down.svg" alt="chevron down" className="w-6 h-6" />
+                    </span>
+                    <span className="flex items-center gap-2">
+                        <span>Font Size</span>
+                        <img src="/chevron-down.svg" alt="chevron down" className="w-6 h-6" />
+                    </span>
+                    <span className="flex items-center gap-2">
+                        <img src="/list.svg" alt="list" className="w-6 h-6" />
+                    </span>
+                    <span className="flex items-center gap-2">
+                        <img src="/link.svg" alt="link" className="w-6 h-6" />
+                    </span>
+                    <span className="self-center h-6 border-r border-black"></span>
+                    <span className="flex items-center gap-2">
+                        <img src="/save.svg" alt="save" className="w-6 h-6" />
+                    </span>
+                    <span className="flex items-center gap-2">
+                        <img src="/refresh-cw.svg" alt="refresh-cw" className="w-6 h-6" />
+                    </span>
+                    <span className="flex items-center gap-2">
+                        <img src="/download.svg" alt="download" className="w-6 h-6" />
+                    </span>
+                    <span className="flex items-center gap-2">
+                        <img src="/settings.svg" alt="settings" className="w-6 h-6" />
+                    </span>
+                    <span className="flex items-center gap-2 text-red-500">
+                        <img src="/log-out.svg" alt="logout" className="w-6 h-6" />
+                    </span>
                 </nav>
             </div>
         </div>
--- sevenworks/src/app/editor/page.tsx
+++ sevenworks/src/app/editor/page.tsx
@@ -2,14 +2,49 @@
 
 import Navbar from "./navbar";
 
-
 export default function Editor() {
-    return (
-      <div className="bg-white min-h-screen p-3">
-        <Navbar />
-        <div className="p-2" />
-        <hr className="border-t border-black w-full p-10" />
-        <h1 className="text-black">Editor</h1>
+  return (
+      <div className="bg-white h-screen p-3 flex flex-col">
+          <Navbar />
+          <div className="flex-1 flex w-full pt-5 gap-4">
+              <div className="flex-none w-[70] h-full p-2 rounded-lg bg-[#E6E6E6]">
+                  {/* vertical stack */}
+                  <div className="flex flex-col gap-4 h-full">
+                        <div className="p-0.5 flex items-center justify-center">
+                            <img src="/user.svg" alt="user" className="w-8 h-8" style={{ filter: 'invert(29%) sepia(14%) saturate(1580%) hue-rotate(175deg) brightness(92%) contrast(88%)' }} />
+                        </div>
+                        <div className="p-0.5 flex items-center justify-center">
+                            <img src="/phone.svg" alt="phone" className="w-8 h-8" style={{ filter: 'invert(29%) sepia(14%) saturate(1580%) hue-rotate(175deg) brightness(92%) contrast(88%)' }} />
+                        </div>
+                        <div className="p-0.5 flex items-center justify-center">
+                            <img src="/book-open.svg" alt="open book" className="w-8 h-8" style={{ filter: 'invert(29%) sepia(14%) saturate(1580%) hue-rotate(175deg) brightness(92%) contrast(88%)' }} />
+                        </div>
+                        <div className="p-0.5 flex items-center justify-center">
+                            <img src="/briefcase.svg" alt="briefcase" className="w-8 h-8" style={{ filter: 'invert(29%) sepia(14%) saturate(1580%) hue-rotate(175deg) brightness(92%) contrast(88%)' }} />
+                        </div>
+                        <div className="p-0.5 flex items-center justify-center">
+                            <img src="/award.svg" alt="award" className="w-8 h-8" style={{ filter: 'invert(29%) sepia(14%) saturate(1580%) hue-rotate(175deg) brightness(92%) contrast(88%)' }} />
+                        </div>
+                        <div className="p-0.5 flex items-center justify-center">
+                            <img src="/plus-square.svg" alt="plus square" className="w-8 h-8" style={{ filter: 'invert(29%) sepia(14%) saturate(1580%) hue-rotate(175deg) brightness(92%) contrast(88%)' }} />
+                        </div>
+                        <div className="mt-auto p-0.5 flex items-center justify-center">
+                            <img src="/chevron-left.svg" alt="chevron left" className="w-8 h-8" style={{ filter: 'invert(29%) sepia(14%) saturate(1580%) hue-rotate(175deg) brightness(92%) contrast(88%)' }} />
+                        </div>
+                  </div>
+              </div>
+              <div className="flex-[1.5] border border-black p-2 rounded-lg">
+                  <h1 className="text-black text-center">Personal Information</h1>
+              </div>
+              <div className="flex-[2] border border-black p-10 rounded-lg" >
+                      <div className="flex items-center justify-center border self-center border-black p-20 h-full w-full flex-row gap-4 rounded-lg">
+                          <h1 className="text-black">Editor Window</h1>
+                      </div>
+              </div>
+              <div className="flex-[2] border border-black p-2 rounded-lg">
+                  <h1 className="text-black">Right</h1>
+              </div>
+          </div>
       </div>
-    );
-  }
+  );
+}
