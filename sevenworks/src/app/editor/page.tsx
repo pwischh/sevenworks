f70e1e9a641dfbcdec1d83735c28d0465bc10857
alexlautin <alexalautin@gmail.com>
2025-03-18 17:37:08 -0400

    fix commit changes

--- sevenworks/src/app/editor/page.tsx
+++ sevenworks/src/app/editor/page.tsx
@@ -11,13 +11,6 @@ import { useAuth } from "../authContext";
 import { useRouter } from "next/navigation";
 
 export default function Editor() {
-<<<<<<< HEAD
-    const [form, setForm] = useState({
-        firstName: "",
-        lastName: "",
-        content: "",
-    });
-=======
 
     const { user, loading } = useAuth();
     const router = useRouter();
@@ -28,15 +21,14 @@ export default function Editor() {
         router.push("/register/login");
       }
     }, [user, loading, router]);
->>>>>>> 60db2736f7db906da6a256e7f6ee56717fa6f2e7
 
     return (
     <div className="bg-white h-screen p-3 flex flex-col">
       <Navbar />
       <div className="flex-1 flex w-full pt-5 gap-2">
         <IconBar />
-        <InputFields form={form} setForm={setForm} />
-        <EditorWindow form={form} />
+        <InputFields />
+        <EditorWindow />
         <RightView />
       </div>
     </div>
